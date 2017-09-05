5bf4fe3ffdbff60bc7c64e92d148028db162c4b9
Hiltunen Joonas <s1601378@student.businesscollege.fi>
2017-09-05 14:10:12 +0300

    Korttipakan hahmottelua

--- Ventti/src/ventti/Ventti.java
+++ Ventti/src/ventti/Ventti.java
@@ -1,22 +1,10 @@
 
 package ventti;
 
-/**
- *
- * @author s1601378
- */
 public class Ventti {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //Moi tää on testi
-        System.out.println("Aika jännä");
-        System.out.println("Testi");
-        //sop
-        //hasul
+        
     }
     
 }
