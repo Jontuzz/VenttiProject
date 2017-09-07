80aca76f451607df8ccfc6b96db4ed7805151a8c
Hiltunen Joonas <s1601378@student.businesscollege.fi>
2017-09-07 13:21:22 +0300

    käyttöliittymä taustalla

--- Ventti/src/ventti/Ventti.java
+++ Ventti/src/ventti/Ventti.java
@@ -3,13 +3,6 @@ package ventti;
 
 public class Ventti {
 
-    public static void main(String[] args) {
-        //luodaan korttipakka olio
-        Korttipakka korttipakka = new Korttipakka();
-        
-        //Tulostetaan korttipakka olio käyttämällä metodia toString()
-        //Sama kuin System.out.println(korttipakka.toString());
-        System.out.println(korttipakka);
-    }
+    
     
 }
