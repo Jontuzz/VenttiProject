77073e2e1391dfc2ae2540642fcd06d8e1958871
Hiltunen Joonas <s1601378@student.businesscollege.fi>
2017-09-09 20:43:21 +0300

    Emännän metodit lisätty

--- Ventti/src/ventti/Emanta.java
+++ Ventti/src/ventti/Emanta.java
@@ -8,4 +8,22 @@ public class Emanta {
     public Emanta() {
         this.emannanPelaamatKortit = new ArrayList<>();
     }
+    
+    public void lisaaKorttiEmanalle(Kortti emannanKortti) {
+        this.emannanPelaamatKortit.add(emannanKortti);
+    }
+    
+    public int getEmannanKorttienSumma() {
+        int korttienSumma = 0;
+        
+        for (Kortti pelaajanKortit : emannanPelaamatKortit) {
+            korttienSumma += pelaajanKortit.getArvo();
+        }
+        
+        return korttienSumma;
+    }
+    
+    public String toString() {
+        return "Emännän korttipakka" + this.emannanPelaamatKortit;
+    }
 }
