c652bbf97b04861fc89ccf2a95eb38b5cb954bfc
Hiltunen Joonas <s1601378@student.businesscollege.fi>
2017-09-10 19:32:40 +0300

    tarkistaVoitto luokan metodi

--- Ventti/src/ventti/TarkistaVoitto.java
+++ Ventti/src/ventti/TarkistaVoitto.java
@@ -1,5 +1,21 @@
 package ventti;
 
 public class TarkistaVoitto {
+    private int emannanKorttienSumma;
+    private int pelaajanKorttienSumma;
+    private int maksimiArvo;
     
+    public TarkistaVoitto(int emannanKorttienSumma, int pelaajanKorttienSumma) {
+        this.emannanKorttienSumma = emannanKorttienSumma;
+        this.pelaajanKorttienSumma = pelaajanKorttienSumma;
+        this.maksimiArvo = 21;
+    }
+    
+    public void kumpiVoitti() {
+        if (this.emannanKorttienSumma > this.pelaajanKorttienSumma && this.emannanKorttienSumma <= this.maksimiArvo) {
+            System.out.println("Emäntä voitti!");
+        } else {
+            System.out.println("Pelaaja voitti");
+        }
+    }
 }
